dc2f80f5d7b1473868cb42812ea2a5c266907131
ekitike22-lfc <khuklaevae@gmail.com>
2026-03-11 17:30:42 +0100

    add age in future program

---
+++ basic-exercises /02_age_In_future.py
@@ -0,0 +1,7 @@
+name = input("Your name? ")
+age = int(input("Your current age? "))
+year_in_future = int(input("How many years in the future? "))
+
+future_age = age + year_in_future
+
+print("Hi,", name, "in", year_in_future, "you will be", future_age, "years old.")
