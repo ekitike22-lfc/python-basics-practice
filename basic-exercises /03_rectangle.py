a2bc36031c5d4ffe5816731bad96dc0b3fe3527d
ekitike22-lfc <khuklaevae@gmail.com>
2026-03-14 17:13:37 +0100

    add rectangle calculator

---
+++ basic-exercises /03_rectangle.py
@@ -0,0 +1,10 @@
+rect_length = int(input("Rectangle length: "))
+rect_width = int(input("Rectangle width: "))
+
+# formulas
+area = rect_width * rect_length
+perimeter = 2 * (rect_length + rec_width)
+
+print("Rectangle dimension:", rect_length, "x", rec_width)
+print("Area:", area)
+print("Perimeter:", perimeter)
